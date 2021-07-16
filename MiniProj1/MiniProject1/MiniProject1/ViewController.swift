67c6ebb8d624e57c0b3e6cf63ea2bb772e27c5fa
sophiashiu <87436863+sophiashiu@users.noreply.github.com>
2021-07-16 09:36:53 -0400

    Add files via upload

---
+++ MiniProj1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Sophia S on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
